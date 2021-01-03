3d5c026354c27491173f51a829e6b50131cca4bf
Glazkov.A <gearpanda@gmail.com>
2021-01-03 22:21:30 +0300

    2020: day 3 both part.

--- 2020/day_03/main.go
+++ 2020/day_03/main.go
@@ -8,23 +8,28 @@ const (
 )
 
 func main() {
-	answer1 := part1()
+	answer1 := findPath(3, 1, len(input[0]))
 	fmt.Println("part 1:", answer1) // 151
+
+	answer2 := findPath(1, 1, len(input[0]))
+	answer2 *= findPath(3, 1, len(input[0]))
+	answer2 *= findPath(5, 1, len(input[0]))
+	answer2 *= findPath(7, 1, len(input[0]))
+	answer2 *= findPath(1, 2, len(input[0]))
+
+	fmt.Println("part 2:", answer2) // 7540141059
 }
 
-func part1() int {
-	var (
-		count int
-		width = len(input[0])
-	)
+func findPath(xi, yi, width int) int {
+	var count int
 
 	for x, y := 0, 0; y < len(input); {
 		if input[y][x] == tree {
 			count++
 		}
 
-		x = (x + 3) % width
-		y += 1
+		x = (x + xi) % width
+		y += yi
 	}
 
 	return count
